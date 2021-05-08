94e7c032dc944d1986a94114fdde3200aea37865
vitovt <sergeevvs92@gmail.com>
2021-05-08 22:48:17 +0300

    Script checks checksum if it doesn't modifaied for 24 h

--- archive_notes.sh
+++ archive_notes.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
-# save checksum
-sha256sum ./daily_notes.txt > ./daily_notes_CHECKSUM 
 
+# save checksum
+
+NOW_TIME=$(date +%s)
+MODIF_TIME=$(stat --format=%Y daily_notes.txt)
+DIF_TIME=24
+DIF_TIME_SEC=$(expr $DIF_TIME \* 60 \* 60 )
+echo $MODIF_TIME
+
+if ! $(sha256sum --status -c ./daily_notes_CHECKSUM)
+then 
+	echo "CHECKSUM doesn't match"
+	if [ $(expr $NOW_TIME - $MODIF_TIME) -gt $DIF_TIME ]
+	then
+		echo "time more than $DIF_TIME hours"
+		echo "------------------------" >> ./archive_notes.txt 
+		cat ./daily_notes.txt >> ./archive_notes.txt
+		sha256sum ./daily_notes.txt > ./daily_notes_CHECKSUM 
+	fi  
+fi
 #chesk chesksum
-sha256sum -c ./daily_notes_CHECKSUM 
+echo $(sha256sum --status -c ./daily_notes_CHECKSUM) 
